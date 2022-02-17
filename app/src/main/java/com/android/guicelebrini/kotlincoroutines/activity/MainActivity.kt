3cad06b0a84e150ee5162303fdead01c778d062d
GuiCelebrini <celebrinig@gmail.com>
2022-02-16 23:07:49 -0300

    Created Character model for Harry Potter characters and implemented Glide library

--- app/src/main/java/com/android/guicelebrini/kotlincoroutines/activity/MainActivity.kt
+++ app/src/main/java/com/android/guicelebrini/kotlincoroutines/activity/MainActivity.kt
@@ -3,8 +3,12 @@ package com.android.guicelebrini.kotlincoroutines.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.guicelebrini.kotlincoroutines.R
+import com.android.guicelebrini.kotlincoroutines.model.Character
 
 class MainActivity : AppCompatActivity() {
+
+    val charactersList = arrayListOf<Character>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/android/guicelebrini/kotlincoroutines/model/Character.kt
@@ -0,0 +1,10 @@
+package com.android.guicelebrini.kotlincoroutines.model
+
+data class Character(
+    val name: String,
+    val house: String,
+    val ancestry: String,
+    val image: String) {
+
+    
+}
